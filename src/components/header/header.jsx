a153ca5fb69bd4886edc7d0572addead2c29af47
John Doe <antoxa106@gmail.com>
2023-02-08 20:56:40 +0200

    comit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -64,7 +64,7 @@ export const Header = observer(()=>{
             <nav className={s.header__menu}>
                 <ul>
                     <li className='header__anim2'><Link to="crypush/">Home</Link></li>
-                    <li className='header__anim3'><Link to="about/about/">About Us</Link></li>
+                    <li className='header__anim3'><Link to="crypush/about/">About Us</Link></li>
                     <li className='header__anim4'><a href="#">Strategies</a></li>
                     <li className='header__anim5'>
                         <span className={s.menu__item}>
